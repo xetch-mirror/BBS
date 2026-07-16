ad6190f53b477c9b0f499d37b777538b6200e07f
Xetch <poopyking482@gmail.com>
2026-07-16 20:53:15 +0800

    Update argument.h

--- Clib/Xlibary/argument.h
+++ Clib/Xlibary/argument.h
@@ -4,15 +4,15 @@
 // argument.h
 // this is part of the Xlibary, everyone can use it
 
-// Provided by kernel's environment / linker
+// provided by kernel's environment / linker
 extern void kernel_print(const char* s);
 extern void kernel_exit(int code);
 
-static inline void print_spacer(void) { kernel_print("  -> "); }
+static inline void print_spacer(void)  { kernel_print("  -> "); }
 static inline void print_newline(void) { kernel_print("\n"); }
-static inline int is_null(const char* str) { return str == 0; }
-static inline int is_empty(const char* str) { return str[0] == '\0'; }
-static inline int check_args(int argc) { return argc > 1; }
+static inline int is_null(const char* str)   { return str == 0; }
+static inline int is_empty(const char* str)  { return str[0] == '\0'; }
+static inline int check_args(int argc)       { return argc > 1; }
 
 static inline void handle_error(void) {
     kernel_print("error: no function provided, exiting\n");
@@ -25,17 +25,12 @@ static inline void output_message(const char* msg) {
     print_newline();
 }
 
+// anyone can call this function from their own main entry point
 static inline void process_command(int argc, char* argv[]) {
     if (!check_args(argc) || is_null(argv[1]) || is_empty(argv[1])) {
         handle_error();
     }
     output_message(argv[1]);
-}
-
-int main(int argc, char* argv[]) {
-    process_command(argc, argv);
-    kernel_exit(0);
-    return 0;
 }
 
 #endif // ARGUMENT_H
