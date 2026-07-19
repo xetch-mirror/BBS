10886f3074792c627c9e6da39c0f3a813ab5a614
Xetch <poopyking482@gmail.com>
2026-07-19 15:45:41 +0800

    Update panic_config.h

--- core/panic_config.h
+++ core/panic_config.h
@@ -1,21 +1,7 @@
 #ifndef PANIC_CONFIG_H
 #define PANIC_CONFIG_H
 
-#include <stdint.h>
-
-// Explicit dimensions based on your OS graphics video mode configuration
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 600
-
-// Panic box properties (Box coordinates centered on screen)
-#define BOX_WIDTH     450
-#define BOX_HEIGHT    180
-#define BOX_X         ((SCREEN_WIDTH - BOX_WIDTH) / 2)
-#define BOX_Y         ((SCREEN_HEIGHT - BOX_HEIGHT) / 2)
-
-// RGB Color Schemes (Assuming 32-bit color depth / 4 bytes per pixel)
-#define COLOR_BLACK   0x00000000
-#define COLOR_WHITE   0x00FFFFFF
-#define COLOR_RED     0x00FF0000
+#define TTY_WIDTH  80
+#define TTY_HEIGHT 25
 
 #endif
