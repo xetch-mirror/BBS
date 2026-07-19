567221592513f05439c140096c69c7ef8943fd4c
Xetch <poopyking482@gmail.com>
2026-07-19 19:21:53 +0800

    Update version.c

--- core/version.c
+++ core/version.c
@@ -1,7 +1,7 @@
 #include "init/sys_io.h"
 
 void version(int argc, char **argv) 
-    io_print("BSS version 1\n"); 
+    io_print("BSS version 0.1\n"); 
     io_print("Bulit by");
     io_print("Xetch");
     // put your name here contributors
