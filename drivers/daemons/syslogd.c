540b292e5fa3690109d61c53bee80481dfac91ea
Xetch <poopyking482@gmail.com>
2026-07-20 17:54:06 +0800

    Create syslogd.c

---
+++ drivers/daemons/syslogd.c
@@ -0,0 +1,21 @@
+#ifndef SYSLOGD_H
+#define SYSLOGD_H
+
+#include "init/include/sys_types.h"
+
+// Log severity levels
+#define LOG_INFO    0
+#define LOG_WARN    1
+#define LOG_ERROR   2
+#define LOG_DEBUG   3
+
+typedef struct {
+    uint8_t level;
+    char origin[16];   // e.g. "mouse_d", "disk_d", "net_d"
+    char message[128];
+} log_event_t;
+
+void syslogd_init(void);
+void syslogd_log(uint8_t level, const char *origin, const char *msg);
+
+#endif
