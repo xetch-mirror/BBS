69eee8554b115ad8c9ba49f5af3b1b57ab1ba789
Xetch <poopyking482@gmail.com>
2026-07-20 18:15:56 +0800

    Create ascii.c

---
+++ core/ascii.c
@@ -0,0 +1,9 @@
+void print_kernel_banner(void) {
+    serial_puts("\n");
+    serial_puts("  ____    ______ ____  \n");
+    serial_puts(" | __ )  / / __ ) ___| \n");
+    serial_puts(" |  _ \\ / /|  _ \\___ \\ \n");
+    serial_puts(" | |_) / / | |_) |__) |\n");
+    serial_puts(" |____/_/  |____/____/ \n");
+    serial_puts("\n");
+}
